cad369dcf3c7f965f71951f63cf61f898237aecb
Bahman Edi Gomari <5835299@RTS-365.dk>
2020-06-04 13:34:37 +0200

    actual fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/button';
+import Text from './components/Text';
+import Something from './components/paragraph';
+import Writing from './components/writing';
+import Image from './components/image';
+import Image2 from './components/image';
+import h1 from './components/h1';
 
-function App() {
-  return (
+ function App() {
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Button/>
+      <Text/>
+      <Something/>
+      <Writing/>
+      <Image/>
+     </div>
+    
   );
 }
 
